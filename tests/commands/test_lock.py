5c40fe69fee11bcc07a1e45e4dca1b531ad9a578
Sébastien Eustace <sebastien@eustace.io>
2017-04-13 18:49:56 -0500

    Adds tests for the lock command

---
+++ tests/commands/test_lock.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+from cleo import CommandTester
+
+
+def test_lock(app, mocker):
+    base_dir = os.path.join(
+        os.path.dirname(__file__), '..', '..', 'examples', 'basic'
+    )
+    lock = os.path.join(base_dir, 'poetry.lock')
+    if os.path.exists(lock):
+        os.unlink(lock)
+
+    getcwd = mocker.patch('os.getcwd')
+    getcwd.return_value = base_dir
+    command = app.find('lock')
+    command_tester = CommandTester(command)
+    command_tester.execute([('command', command.name)])
+
+    output = command_tester.get_display()
+    expected = """
+Locking dependencies to poetry.lock
+
+  - Resolving dependencies
+  - Writing dependencies
+"""
+
+    assert expected == output
